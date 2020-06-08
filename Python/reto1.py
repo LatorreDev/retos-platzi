d66fead4eb598fa41efbc1be80fee9f7bb2444c8
David Latorre <david@latorredev.com>
2020-06-08 16:32:46 -0500

    [FIX] Fix pep 8

--- Python/reto1.py
+++ Python/reto1.py
@@ -1,10 +1,12 @@
 #!/usr/bin/env python3
 # Retos platzi parte 1
 
+
 def linea_div():
     print("\n")
     print("***************************")
     print("\n")
+
 
 # Hola mundo
 
@@ -15,14 +17,16 @@ print("Hola mundo")
 linea_div()
 
 # Hola nombre y apellido
-nombre=input("Cual es tu nombre?: ")
-apellido=input("Cual es tu apellido?: ")
-print(f"Hola,{nombre} {apellido}")
+nombre = input("Cual es tu nombre?: ")
+apellido = input("Cual es tu apellido?: ")
+print(f"Hola, {nombre} {apellido}")
 
 linea_div()
 
 # Categorias Platzi
-categorias = ["[Desarrollo e ingenieria]","[Diseño y UX]","[Marketing]","[Negocios y Emprendieminto]", "[Produccion Audiovisual]", "[Crecimiento Profesional]"]
+categorias = ["[Desarrollo e ingenieria]", "[Diseño y UX]", "[Marketing]",
+              "[Negocios y Emprendieminto]", "[Produccion Audiovisual]",
+              "[Crecimiento Profesional]"]
 print(f"Platzi cuenta con cursos de: ")
 for elements in categorias:
     print(elements)

96e6c0efae2df91bf770c5fb09a3b5b73f660001
David Latorre <david@latorredev.com>
2020-06-07 21:38:04 -0500

    [ADD] add 3 first exercises for platzi retos

---
+++ Python/reto1.py
@@ -0,0 +1,30 @@
+#!/usr/bin/env python3
+# Retos platzi parte 1
+
+def linea_div():
+    print("\n")
+    print("***************************")
+    print("\n")
+
+# Hola mundo
+
+linea_div()
+
+print("Hola mundo")
+
+linea_div()
+
+# Hola nombre y apellido
+nombre=input("Cual es tu nombre?: ")
+apellido=input("Cual es tu apellido?: ")
+print(f"Hola,{nombre} {apellido}")
+
+linea_div()
+
+# Categorias Platzi
+categorias = ["[Desarrollo e ingenieria]","[Diseño y UX]","[Marketing]","[Negocios y Emprendieminto]", "[Produccion Audiovisual]", "[Crecimiento Profesional]"]
+print(f"Platzi cuenta con cursos de: ")
+for elements in categorias:
+    print(elements)
+
+linea_div()
